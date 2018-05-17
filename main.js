2fb0ddd09fddc31ee4b7aab8c34b556ab1199655
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2018-05-17 17:42:24 -0300

    feat: Initial working commit

--- main.js
+++ main.js
@@ -3,41 +3,67 @@
 window.onload = function() {
 
     function preproc_data(dataset) {
-        let attrs = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'];
+        // let attrs = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'];
+        let attrs = dataset.columns;
 
+        let d1 = dataset[0];
         let scales = {};
         for (const attr of attrs) {
-            if (isNaN(+d[attr])) {
-                scales[attr] = d3.scalePoint().domain(d3.map(dataset, function(d){return d.species;}).keys()).range([480, 20])
+            if (isNaN(+d1[attr])) {
+                // scales[attr] = d3.scalePoint().domain(d3.map(dataset, function(d){return d[attr];}).keys()).range([480, 20]);
+                scales[attr] = d3.scalePoint().domain(d3.map(dataset, function(d){return d[attr];}).keys()).range([480, 20]);
             } else {
-                scales[attr] = d3.scaleLinear().domain([0,d3.max(dataset)]).range([480, 20])
+                // scales[attr] = d3.scaleLinear().domain([0,d3.max(dataset[attr])]).range([480, 20]);
+                scales[attr] = d3.scaleLinear().domain(d3.extent(dataset, (d) => {return d[attr];})).range([480, 20]);
             }
         }
 
-        function scaleX() {
-            return d3.scaleOrdinal().domain(attrs).range([20, 580]);
-        }
+        let scaleX = d3.scalePoint().domain(attrs).range([20, 580]);
 
-        function drawLine(d, i) {
-            let str = "M "
+        function drawLine(d) {
+            let str = 'M ';
             for (const attr of attrs) {
-                str += scaleX(attr) + " " + scales[attr](d[attr]) + " L ";
+                str += scaleX(attr) + ' ' + scales[attr](d[attr]) + ' L ';
             }
             return str.slice(0, -3);
         }
+        
+        function handleMouseOver(d) {
+            console.log("Hi");
+            d3.select(this).attr('stroke', 'orange');
+        }
 
-        d3.select('mysvg').selectAll("path")
-        .data(dataset).enter()
-        .append('path')
-        .attr('d', drawLine(d, i))
-        .style('fill', 'none')
-        .style('stroke', 'lightblue')
+        function handleMouseOut(d) {
+            d3.select(this).attr('stroke', 'black');
+        }
+
+        d3.select('#mysvg').selectAll('path.dataPath')
+            .data(dataset)
+            .enter()
+            .append('path')
+            .attr('class', 'dataPath')
+            .attr('d', drawLine)
+            .attr('pointer-events', 'visibleStroke')
+            // .style('fill', 'none')
+            // .style('stroke', 'lightblue')
+            .on('mouseover', handleMouseOver)
+            .on('mouseout', handleMouseOut);
+        
+        d3.axisLeft(scales.sepal_width)
+        d3.select('#mysvg').selectAll('g.axisGroup')
+            .data(attrs).enter()
+            .append('g')
+            .attr('class', 'axisGroup')
+            .attr('transform', function(d){
+                return 'translate('+scaleX(d)+',20)';
+            })
+            .call();
     }
 
     function render(data){
-        d3.select("body")
-          .append("pre")
-          .text(JSON.stringify(data, null, 2));
+        d3.select('body')
+            .append('pre')
+            .text(JSON.stringify(data, null, 2));
     }
 
     function type(d){
@@ -49,8 +75,6 @@ window.onload = function() {
         return d;
     }
 
-    d3.csv("iris.csv", type).then(
-        
-        preproc_data(dataset)
-    );
+    d3.csv('iris.csv', type)
+        .then(preproc_data);
 };
